e492b3fbca97c08f6682c438b270dea121490996
Sheik Althaf <althafmech@outlook.com>
2019-02-10 18:12:13 +0530

    fix the build failed

--- projects/datepicker-demo/src/app/app.module.ts
+++ projects/datepicker-demo/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { DatepickerModule } from 'ngu-datepicker';
+import { DatepickerModule } from '../../../ngu-datepicker/src/public_api';
 
 @NgModule({
   declarations: [AppComponent],
